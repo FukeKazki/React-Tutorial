379a48174d1f5213de07638d6eb7b6a21c1f9bb3
FukeKazki <kazkichi0906@gmail.com>
2020-12-24 00:32:48 +0900

    Update: 最低限のTODOリストの作成 (#1)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import  './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    // タスクを保持するState
+    const [tasks, setTasks] = useState([])
+    // 入力された文字を保持するState
+    const [taskTitle, setTaskTitle] = useState('')
+
+    // 文字が入力されるたびに呼ばれる
+    const handleTaskTitle = (e) => {
+        setTaskTitle(e.target.value)
+    }
+
+    // 入力した文字を空にする
+    const resetTaskTitle = () => {
+        setTaskTitle('')
+    }
+
+    // 入力された文字をタスクに追加する
+    const addTask = () => {
+        setTasks(prev => [
+            ...prev,
+            {
+                title: taskTitle
+            }
+        ])
+    }
+
+    // ボタンを押したときに呼ばれる
+    const handleAddButton = () => {
+        addTask()
+        resetTaskTitle()
+    }
+
+    return (
+        <>
+            {/* 入力フォーム */}
+            <div className='form'>
+                <label>タスク名: </label>
+                <input onChange={handleTaskTitle} value={taskTitle}/>
+                <button onClick={handleAddButton}>追加する</button>
+            </div>
+
+            {/* タスクリスト */}
+            <div className='task-list'>
+                {/* 配列の中身の数だけ繰り返す */}
+                {tasks.map(task => (
+                    <div key={task.title} className='task'>
+                        <h3>{task.title}</h3>
+                    </div>
+                ))}
+            </div>
+        </>
+    )
 }
 
-export default App;
+export default App
